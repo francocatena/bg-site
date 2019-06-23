7e05c952f54ecadf603ba086ca98a2442397546c
Franco Catena <francocatena@gmail.com>
2019-06-22 23:28:23 -0300

    Add contact form + Little restyling

--- js/main.js
+++ js/main.js
@@ -2,4 +2,15 @@
   $(document).on('click', '.navbar-collapse a', function () {
     $('.navbar-collapse').collapse('hide')
   })
+
+  $(document).on('submit', 'form.needs-validation', function (event) {
+    var form = event.currentTarget
+
+    if (form.checkValidity() === false) {
+      event.preventDefault()
+      event.stopPropagation()
+    }
+
+    $(form).addClass('was-validated')
+  })
 }()
